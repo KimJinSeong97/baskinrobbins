435171d7c1ac531f932864d5b2fc3bef8dabf81d
KimJinSeong97 <100419154+KimJinSeong97@users.noreply.github.com>
2022-05-26 19:02:48 +0900

    220526
    responsive

--- javascript/index.js
+++ javascript/index.js
@@ -6,32 +6,60 @@ $(function () {
         $('.sub_menu').stop().slideUp();
     });
 
-// monthly best 슬라이드
-$('.prev').click(function () {
-    $('.flavors li:last').prependTo('.flavors');
-    $('.flavors').css('margin-left', -400);
-    $('.flavors').stop().animate({ marginLeft: 0 }, 800)
-});
+    // monthly best 슬라이드
+    // $('.prev').click(function () {
+    //     $('.flavors li:last').prependTo('.flavors');
+    //     $('.flavors').css('margin-left', -400);
+    //     $('.flavors').stop().animate({ marginLeft: 0 }, 800)
+    // });
 
-$('.next').click(function () {
-    $('.flavors').stop().animate({ marginLeft: -400 }, 800, function () {
-        $('.flavors li:first').appendTo('.flavors');
-        $('.flavors').css({ marginLeft: 0 });
+    // $('.next').click(function () {
+    //     $('.flavors').stop().animate({ marginLeft: -400 }, 800, function () {
+    //         $('.flavors li:first').appendTo('.flavors');
+    //         $('.flavors').css({ marginLeft: 0 });
+    //     });
+    // });
+
+    // rating 슬라이드
+    $('.prev2').click(function () {
+        $('#rating_box li:last').prependTo('#rating_box');
+        $('#rating_box').css('margin-left', -600);
+        $('#rating_box').stop().animate({ marginLeft: 0 }, 800);
     });
-});
 
-// rating 슬라이드
-$('.prev2').click(function () {
-    $('#rating_box li:last').prependTo('#rating_box');
-    $('#rating_box').css('margin-left', -600);
-    $('#rating_box').stop().animate({ marginLeft: 0 }, 800);
-});
-
-$('.next2').click(function () {
-    $('#rating_box').stop().animate({ marginLeft: -600 }, 800, function () {
-        $('#rating_box li:first').appendTo('#rating_box');
-        $('#rating_box').css({ marginLeft: 0 });
+    $('.next2').click(function () {
+        $('#rating_box').stop().animate({ marginLeft: -600 }, 800, function () {
+            $('#rating_box li:first').appendTo('#rating_box');
+            $('#rating_box').css({ marginLeft: 0 });
+        });
     });
-});
 
+
+    if (window.matchMedia("(min-width:1200px)").matches) {
+        $('.prev').click(function () {
+            $('.flavors li:last').prependTo('.flavors');
+            $('.flavors').css('margin-left', -400);
+            $('.flavors').stop().animate({ marginLeft: 0 }, 800)
+        });
+
+        $('.next').click(function () {
+            $('.flavors').stop().animate({ marginLeft: -400 }, 800, function () {
+                $('.flavors li:first').appendTo('.flavors');
+                $('.flavors').css({ marginLeft: 0 });
+            });
+        });
+    } else if (window.matchMedia("(min-width:768px) and (max-width:1199px)").matches) {
+        $('.prev').click(function () {
+            $('.flavors li:last').prependTo('.flavors');
+            $('.flavors').css('margin-left', "-31.5%");
+            $('.flavors').stop().animate({ marginLeft: 0 }, 800)
+        });
+
+        $('.next').click(function () {
+            $('.flavors').stop().animate({ marginLeft: "-31.5%" }, 800, function () {
+                $('.flavors li:first').appendTo('.flavors');
+                $('.flavors').css({ marginLeft: 0 });
+            });
+        });
+    }
 });
