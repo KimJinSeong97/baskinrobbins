a644d5d47ef53340d1f49caadf61c854c5b35149
KimJinSeong97 <100419154+KimJinSeong97@users.noreply.github.com>
2022-06-07 18:21:17 +0900

    220607
    sub 1차 완료

--- javascript/sub.js
+++ javascript/sub.js
@@ -1,3 +1,4 @@
+// nav 펼치는 스크립트
 $(function () {
     $('.menu>li').mouseover(function () {
         $('.sub_menu').stop().slideDown();
@@ -6,38 +7,12 @@ $(function () {
     });
 });
 
-var banner_left = 0;
-var imgCnt = 0;
-var first = 1;
-var last;
-var interval;
-$(document).ready(function () {
-    $('.slides a').each(function () {
-        $(this).css('left', banner_left);
-        banner_left += $(this).width() + 5; //위치값
-        $(this).attr('id', 'content' + (++imgCnt));
+// 이미지 클릭시 새창 나오는 스크립트
+$(function () {
+    $('#instagram img').click(function () {
+        $('#open').css("display", "flex");
     });
-    last = imgCnt;
-    rollingStart();
+    $('#exit').click(function () {
+        $('#open').css("display", "none");
+    });
 });
-
-function rollingStart() {
-    interval = setInterval(function () {
-        $('.slides a').each(function () {
-            $(this).css('left', $(this).position().left - 1);
-        });
-        var first_content = $('#content' + first);
-        var last_content = $('#content' + last);
-        if (first_content.position().left < '-' + $(first_content).width()) {
-            first_content.css('left', last_content.position().left + last_content.width() + 5);
-            first++;
-            last++;
-            if (last > imgCnt) {
-                last = 1;
-            }
-            if (first > imgCnt) {
-                first = 1;
-            }
-        }
-    }, 10);
-}
